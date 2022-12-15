d114edf859ff99117303c42b58f3369ca9d19ed1
JacksonMuriithi <jacksonmuriithi299@mail.com>
2022-12-15 13:07:30 +0300

    completed lab

--- index.js
+++ index.js
@@ -23,3 +23,44 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+function reverseString(word) {
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord
+}
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+
+  if(word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+
+  // Write your algorithm here
+
+}
+
+/* 
+  Add your pseudocode here
+  if the word is the same as in reverse order then return true, if not return false.
+*/
+
+/*
+  Add written explanation of your solution here
+*/
+
+// You can run `node index.js` to view these console logs
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
+
+module.exports = isPalindrome;
